aab3ddc9e7b6aaee3b33ade71663ede414e59b91
kai tran <ultra1524@gmail.com>
2024-04-08 16:46:32 -0700

    Initial Commit

---
+++ CleaningApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  CleaningApp
+//
+//  Created by Triet Tran on 4/8/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
