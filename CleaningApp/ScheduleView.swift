dc2a944dd794a9584fca3bdc892098086e62e2d1
kai tran <ultra1524@gmail.com>
2024-04-15 05:13:24 -0700

    add button to taskview

---
+++ CleaningApp/ScheduleView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ScheduleView: View {
+    var body: some View {
+        Text("This is the Schedule page")
+    }
+}
+
+struct ScheduleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScheduleView()
+    }
+}
