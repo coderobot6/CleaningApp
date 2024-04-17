b73f0531887f93d1677a225f1abfca9ad6d18df8
kai tran <ultra1524@gmail.com>
2024-04-17 01:50:37 -0700

    remove some .xcodeproj folder

--- CleaningApp/CleaningApp.swift
+++ CleaningApp/CleaningApp.swift
@@ -7,21 +7,11 @@
 
 import SwiftUI
 import SwiftData
-import FirebaseCore
 
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        FirebaseApp.configure()
-        return true
-    }
-}
 
 
 @main
 struct CleaningApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
             Item.self,
